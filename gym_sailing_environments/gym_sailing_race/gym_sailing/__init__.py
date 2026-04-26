8c0f4d15186a9a6d63680d5145680016cc041a4b
Nicole Frank <nfrank6@depaul.edu>
2026-04-26 14:42:24 -0500

    :reset my attempt at a race environment, it wasn't working

--- gym_sailing_environments/gym_sailing_race/gym_sailing/__init__.py
+++ gym_sailing_environments/gym_sailing_race/gym_sailing/__init__.py
@@ -1,11 +1,19 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="SailboatOlympicTriangle-v0",
-    entry_point="gym_sailing.envs:SailboatOlympicTriangleEnv",
+    id="Sailboat-v0",
+    entry_point="gym_sailing.envs:SailboatEnv",
+    max_episode_steps=3000,
 )
 
 register(
-    id="SailboatOlympicTriangleDiscrete-v0",
-    entry_point="gym_sailing.envs:SailboatOlympicTriangleDiscreteEnv",
+    id="SailboatDiscrete-v0",
+    entry_point="gym_sailing.envs:SailboatDiscreteEnv",
+    max_episode_steps=3000,
+)
+
+register(
+    id="Motorboat-v0",
+    entry_point="gym_sailing.envs:MotorboatEnv",
+    max_episode_steps=2000,
 )
