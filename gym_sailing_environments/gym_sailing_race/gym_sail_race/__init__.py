c445b33976438ab6e8a1b78e75d795166e677c4f
Nicole Frank <nfrank6@depaul.edu>
2026-04-28 16:43:44 -0500

    made the length of an ep in race even longer through registration

--- gym_sailing_environments/gym_sailing_race/gym_sail_race/__init__.py
+++ gym_sailing_environments/gym_sailing_race/gym_sail_race/__init__.py
@@ -3,7 +3,7 @@ from gymnasium.envs.registration import register
 register(
     id="SailboatRace-v0",
     entry_point="gym_sail_race.envs:SailboatRaceEnv",
-    max_episode_steps=15000,
+    max_episode_steps=20000,
 )
 
 register(
